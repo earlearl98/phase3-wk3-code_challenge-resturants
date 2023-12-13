bc02f7b617765c4a6a4645387c0f9aa53c614330
earlearl98 <earlearl1720@gmail.com>
2023-12-13 08:55:37 +0300

    Completed assignment

--- lib/seed.py
+++ lib/seed.py
@@ -73,73 +73,7 @@ if __name__ == '__main__':
 
 
 
-# from faker import Faker
-# import random
 
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-
-# from models import Customer, Restaurant, Review
-
-
-# if __name__ == '__main__':
-#     engine = create_engine('sqlite:///restaurants.sqlite')
-#     Session = sessionmaker(bind=engine)
-#     session = Session()
-
-#     session.query(Customer).delete()
-#     session.query(Restaurant).delete()
-#     session.query(Review).delete()
-
-#     fake = Faker()
-
-#     customers = []
-#     for i in range(20):
-#       customer = Customer(
-#         first_name =fake.first_name(), 
-#         last_name= fake.last_name()
-#         )
-
-#     session.add(customer)
-#     session = sessionmaker(bind=engine)
-#     session = session()
-
-#     session.query(Customer).delete()
-#     session.query(Restaurant).delete()
-#     session.commit()   
-#     customers.append(customer)
-    
-
-#     restaurants = []
-#     for i in range(10):
-#       resturant = Restaurant(
-#         name=fake.company(),
-#         price=random.randint(1000,20000))
-
-#       session.add(resturant)
-#       session.commit()  
-#       restaurants.append(resturant)
-
-
-#     reviews = []
-#     for customer in customers:
-#       for restaurant in restaurants:
-        
-#         star_rating = random.randint(1, 5)
-        
-#         review = Review(
-#             star_rating=random.SystemRandom().uniform(5, 10),
-#             customer_id=customer.id,
-#             restaurant_id=restaurant.id,
-#         )
-        
-        
-#         reviews.append(review)
-
-
-#     session.add_all(reviews)
-#     session.commit()
-#     session.close()
 
 
 
